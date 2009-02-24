b72d321f2de40a0be1fc07a618704960dad23e92
mviswana@usc.edu <mviswana@usc.edu@8cd62eba-01cd-11de-994d-8308d8631604>
2009-02-24 05:45:41 +0000

    Added code to retrieve the thumbnails from the Android MediaStore and display the IDs of the corresponding images in a grid view.
    Need to change the display of the IDs to displaying the actual
    thumbnails. This will probably require a custom adapter derived from
    SimpleCursorAdapter (see http://groups.google.com/group/android-developers/browse_thread/thread/347c5df5a13fec58).
    
    There are some samples in the ApiDemos that illustrate writing custom
    adapters for displaying images. But the tutorial at
    http://android.ifies.org/tutorial/2009/02/gallery-tutorial-file-location
    explains things a little better. Though it uses a gallery rather than a
    grid view, it should still be applicable.

--- trunk/src/cs546/group7/AssignmentThree.java
+++ trunk/src/cs546/group7/AssignmentThree.java
@@ -31,9 +31,24 @@ package cs546.group7 ;
 
 //------------------------------ IMPORTS --------------------------------
 
+// Android UI support
+import android.widget.SimpleCursorAdapter ;
+import android.widget.CursorAdapter ;
+import android.widget.GridView ;
+
+// Android content-provider support
+import android.provider.MediaStore.Images.Thumbnails ;
+
+// Android database support
+import android.database.Cursor ;
+
 // Android application and OS support
 import android.app.Activity ;
 import android.os.Bundle ;
+
+// Android utilities
+import android.content.ContentUris ;
+import android.util.Log ;
 
 //------------------------- CLASS DEFINITION ----------------------------
 
@@ -47,6 +62,10 @@ import android.os.Bundle ;
 */
 public class AssignmentThree extends Activity {
 
+// The main screen of the photo manager application shows thumbnails of
+// all the available images in a neat grid.
+private GridView m_thumbnails_grid ;
+
 //-------------------------- INITIALIZATION -----------------------------
 
 /**
@@ -57,9 +76,59 @@ public class AssignmentThree extends Activity {
 {
    super.onCreate(saved_state) ;
    setContentView(R.layout.main) ;
+
+   // Setup the thumbnails grid
+   m_thumbnails_grid = (GridView) findViewById(R.id.thumbnails_grid) ;
+
+   // Display the thumbnails of all available images
+   display_thumbnails(m_thumbnails_grid) ;
 }
+
+//------------------------- IMAGE THUMBNAILS ----------------------------
+
+// Display the available thumbnails in the specified grid view
+private void display_thumbnails(GridView G)
+{
+   String[] from = new String[] {Thumbnails.IMAGE_ID} ;
+   int[]    to   = new int[]    {R.id.thumbnail} ;
+   G.setAdapter(new SimpleCursorAdapter(this, R.layout.thumbnail,
+                                        get_thumbnails(), from, to)) ;
+}
+
+// Retrieve the available thumbnails
+private Cursor get_thumbnails()
+{
+   try
+   {
+      String[] columns = new String[] {
+         Thumbnails._ID,
+         Thumbnails.DATA,
+         Thumbnails.IMAGE_ID,
+      } ;
+      return managedQuery(Thumbnails.EXTERNAL_CONTENT_URI, columns,
+                          null, null, null) ;
+   }
+   catch (android.database.sqlite.SQLiteException e)
+   {
+      Log.e(null, "MVN: unable to retrieve thumbnails", e) ;
+      return null ;
+   }
+}
+
+//----------------------- IMAGE CURSOR ADAPTER --------------------------
+
+/**
+   This inner class connects the thumbnails obtained from the Android
+   MediaStore to the grid view used to display the thumbnails on the main
+   screen of the photo manager application.
+*/
+/*
+private class ImageCursorAdapter extends CursorAdapter {
+   public ImageCursorAdapter(Context context, Cursor cursor) {
+   }
+} // end of inner class ImageCursorAdapter
+//*/
 
 //-----------------------------------------------------------------------
 
 } // end of class AssignmentThree
-
