1b2535f5ed5ada2b580ffb0cfc838b037b046971
mviswana@usc.edu <mviswana@usc.edu@8cd62eba-01cd-11de-994d-8308d8631604>
2009-02-24 22:27:51 +0000

    1. Added a click listener to the main screen's grid view to respond to    user selections of thumbnails. For now, this handler is a stub that    simply prints a notification stating the ID of the thumbnail whose    image will be displayed in a new activity.
    2. Added a utilities class to collect various utility routine. For now,
       this class only defines the notification function mentioned above.
    
    3. Removed the main screen's top-level linear layout UI element. Earlier,
       the plan was to put a button below the grid view for adding new
       pictures to the collection. But on the phone, it might just be easier
       to press the MENU button and select an action from there instead of
       having to scroll through all the thumbnails to get to the button.
    
       So now, the main screen's UI consists solely of a grid view without
       any encompassing layout.
    
    4. Fixed errors in the GPL copyright notices in all the XML files. XML is
       really finicky about hyphens in comments; it just can't stand 'em!
    
       More importantly: I really should build and test before checking code
       into the repository... :)

---
+++ src/cs546/group7/Utils.java
@@ -0,0 +1,80 @@
+/*
+   *******************************************************************
+   *                                                                 *
+   *    CSCI-546, Spring 2009 Assignment III Solution by Group #7    *
+   *                                                                 *
+   *    Group Members: Chang, Chin-Kai      chinkaic@usc.edu         *
+   *                   Moon, Ji Hyun        jihyunmo@usc.edu         *
+   *                   Patlolla, Avinash    patlolla@usc.edu         *
+   *                   Viswanathan, Manu    mviswana@usc.edu         *
+   *                                                                 *
+   *******************************************************************
+   *                                                                 *
+   * This file contains an assortment of utility functions used by   *
+   * different parts of the photo manager application.               *
+   *                                                                 *
+   *******************************************************************
+*/
+
+/*
+   AssignmentThree -- a photo manager application for the Google Phone
+
+   Copyright (C) 2009 Chin-Kai Chang
+                      Ji Hyun Moon
+                      Avinash Patlolla
+                      Manu Viswanathan
+
+   This file is part of AssignmentThree.
+
+   AssignmentThree is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   AssignmentThree is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with AssignmentThree; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
+*/
+
+/*
+  REVISION HISTORY
+
+  $HeadURL$
+  $Id$
+*/
+
+//----------------------- PACKAGE SPECIFICATION -------------------------
+
+package cs546.group7 ;
+
+//------------------------------ IMPORTS --------------------------------
+
+// Android UI support
+import android.widget.Toast ;
+
+// Android application and OS support
+import android.content.Context ;
+
+//------------------------- CLASS DEFINITION ----------------------------
+
+/**
+   This class provides several handy utility functions.
+*/
+public class Utils {
+
+//------------------------- UI NOTIFICATIONS ----------------------------
+
+public final static void notify(Context C, String msg)
+{
+   Toast.makeText(C, msg, Toast.LENGTH_SHORT).show() ;
+}
+
+//-----------------------------------------------------------------------
+
+} // end of class cs546.group7.Utils
