b3eaf2318140e26bb1f0952a3d0d6f9e14cf689d
mviswana@usc.edu <mviswana@usc.edu@8cd62eba-01cd-11de-994d-8308d8631604>
2009-02-28 17:50:23 +0000

    1. Updated the main activity to use a sub-activity for previewing and    capturing images from the camera. The earlier design assumed this    would happen with a helper class. However, the Android paradigm    requires user interactions to take place via separate activities that    invoke each other by passing intents around. Consequently, the old    approach would just not have worked.
    2. Totally revamped the ImageRecorder class to function as a separate
       camera preview-and-capture activity rather than a helper class
       derived from Recorder. As mentioned above, that would not have
       worked. This sub-activity captures an image from the camera and
       stores it to the database. It also records the GPS location of the
       image and does a few other related things. Finally, it returns the ID
       of the thumbnail of the new image to the main activity.
    
       NOTE: All of this functionality is not yet in. There are still some
       TODOs in ImageRecorder.java!
    
    3. Added the specs for the camera preview-and-capture activity to the
       Android manifest.
    
    4. Removed GPSRecorder.java as it is quite useless. The camera
       preview-and-capture activity takes care of this now. There's no need
       for an entire class for this functionality as it simply involves two
       Android API calls.
    
       NOTE: Although the Recorder based hierarchy is now defunct, left
       Recorder.java alone for now to prevent builds from breaking. Once the
       AudioRecorder is properly implemented, Recorder.java will be removed.
    
    5. Removed ImageCaptureCallback.java. This class is now directly rolled
       into ImageRecorder.

--- trunk/src/cs546/group7/ImageRecorder.java
+++ trunk/src/cs546/group7/ImageRecorder.java
@@ -19,29 +19,29 @@
  */
 
 /*
- AssignmentThree -- a photo manager application for the Google Phone
+   AssignmentThree -- a photo manager application for the Google Phone
 
- Copyright (C) 2009 Chin-Kai Chang
- Ji Hyun Moon
- Avinash Patlolla
- Manu Viswanathan
+   Copyright (C) 2009 Chin-Kai Chang
+                      Ji Hyun Moon
+                      Avinash Patlolla
+                      Manu Viswanathan
 
- This file is part of AssignmentThree.
+   This file is part of AssignmentThree.
 
- AssignmentThree is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License as
- published by the Free Software Foundation; either version 2 of the
- License, or (at your option) any later version.
+   AssignmentThree is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
 
- AssignmentThree is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- General Public License for more details.
+   AssignmentThree is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with AssignmentThree; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- USA.
+   You should have received a copy of the GNU General Public License
+   along with AssignmentThree; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
  */
 
 /*
@@ -52,109 +52,175 @@
  */
 
 //----------------------- PACKAGE SPECIFICATION -------------------------
-package cs546.group7;
+
+package cs546.group7 ;
 
 //------------------------------ IMPORTS --------------------------------
 
-// Android application and OS support
-import java.io.FileNotFoundException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+// Android camera support
+import android.hardware.Camera ;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.hardware.Camera;
-import android.net.Uri;
-import android.provider.MediaStore.Images.Media;
-import android.util.Log;
+// Android UI support
+import android.view.SurfaceView ;
+import android.view.SurfaceHolder ;
+import android.view.Window ;
+
+import android.view.KeyEvent ;
+
+// Android graphics support
+import android.graphics.BitmapFactory ;
+import android.graphics.Bitmap ;
+
+// Android "IPC" support
+import android.provider.MediaStore.Images.Media ;
+import android.content.Intent ;
+
+// Android networking support
+//import android.net.Uri ;
+
+// Android application and OS support
+import android.app.Activity ;
+import android.content.Context ;
+import android.os.Bundle ;
 
 //------------------------- CLASS DEFINITION ----------------------------
 
 /**
- * This class performs the necessary interfacing rituals with Android in order
- * to capture a new picture using the gPhone's on-board camera and store it to
- * the MediaStore.Images.Media database.
- */
-class ImageRecorder extends Recorder {
+   This class performs the necessary interfacing rituals with Android in
+   order to capture a new picture using the gPhone's on-board camera and
+   store it to the MediaStore.Images.Media database.
+*/
+public class ImageRecorder extends Activity {
 
-	private Camera camera;
-	private SimpleDateFormat timeStampFormat = new SimpleDateFormat("HHmmssSS");
+// Since this class's raison d'etre is to capture images from the camera,
+// it shouldn't be surprising that we need this thing:
+private Camera m_camera ;
 
-	// / The constructor expects to be passed a viable Android context
-	public ImageRecorder(Context C) {
-		super(C);
-		
-	}
+// Once this activity has captured a new picture from the camera, it will
+// insert it into the MediaStore.Images.Media database and return the ID
+// of the corresponding thumbnail to the main activity.
+private long m_thumbnail_id = -1 ;
 
-	// / This method captures an image from the phone's camera
-	@Override
-	public void capture() {
-		// KAI ==> REPLACE THIS AND THE NEXT LINE WITH CAMERA CAPTURE CODE
+//-------------------------- INITIALIZATION -----------------------------
 
-	}
+/**
+   This method is called when the activity is first created. It is akin
+   to the "main" function in normal desktop applications.
+*/
+@Override public void onCreate(Bundle saved_state)
+{
+   super.onCreate(saved_state) ;
+   requestWindowFeature(Window.FEATURE_NO_TITLE) ;
+   setContentView(new CameraPreview(this)) ;
+}
 
-	Camera.PictureCallback mPictureCallbackRaw = new Camera.PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera c) {
-			Log.e(getClass().getSimpleName(), "PICTURE CALLBACK RAW: " + data);
-			// camera.startPreview();
-		}
-	};
+//----------------------------- CLEAN-UP --------------------------------
 
-	Camera.PictureCallback mPictureCallbackJpeg = new Camera.PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera c) {
-			Log.e(getClass().getSimpleName(),
-					"PICTURE CALLBACK JPEG: data.length = " + data);
-			// camera.startPreview();
-		}
-	};
+// The photo manager app's main activity invokes this one and expects it
+// to pass back the ID of the newly captured picture's thumbnail. This
+// method performs the necessary incantations required to communicate
+// this information back to the main activity.
+private void wind_up(int result_code)
+{
+   Bundle B = new Bundle() ;
+   B.putLong(AssignmentThree.EXTRAS_THUMBNAIL_ID, m_thumbnail_id) ;
 
-	Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
-		public void onShutter() {
-			Log.d(getClass().getSimpleName(), "SHUTTER CALLBACK");
-		}
-	};
+   Intent I = new Intent() ;
+   I.putExtras(B) ;
+   setResult(result_code, I) ;
 
-	// / This method stores the image captured from the camera to the
-	// / MediaStore.Images.Media database and returns the ID of the newly
-	// / added picture.
-	@Override
-	public long store() {
-		// KAI ==> REPLACE THIS AND THE NEXT TWO LINES WITH IMAGE STORING CODE
-		ImageCaptureCallback iccb = null;
-		int picture_id = -1;
-		Utils.notify(getContext(), "Storing new picture in database...");
-		// This is done by onPictureTaken.....
-		Utils.notify(getContext(), "Capturing image from camera...");
-		String filename = "my" + timeStampFormat.format(new Date());
-		try {
-			ContentValues values = new ContentValues();
-			values.put(Media.TITLE, filename);
-			values.put(Media.DESCRIPTION, "Image capture by camera");
-			Log.w("capture", "try insert ext uri");			
-			Uri uri = getContext().getContentResolver().insert(Media.EXTERNAL_CONTENT_URI,
-					values);
-			picture_id = Integer.parseInt(uri.getLastPathSegment());
-			Log.w("capture", " pic id =" + picture_id);
+   finish() ;
+}
 
-			iccb = new ImageCaptureCallback(getContext().getContentResolver()
-					.openOutputStream(uri));
-			Log.w("capture", "After ICCB");
-			camera = Camera.open();
-			camera.takePicture(mShutterCallback, mPictureCallbackRaw, iccb);
-		} catch (FileNotFoundException fnfe) {
-			Log.e("capture", filename + " File Not found");
-			Log.e(getClass().getSimpleName(), fnfe.getMessage(), fnfe);
-		} catch (NullPointerException npe) {
-			Log.e(getClass().getSimpleName(), npe.getMessage(), npe);
-		} catch (Exception e) {
-			Log.e(getClass().getSimpleName(), e.getMessage(), e);
-			
-		}
+//-------------------------- KEYBOARD EVENTS ----------------------------
 
-		return picture_id;
+/**
+   This method is called when the activity receives a keyboard event. In
+   the case of this activity, i.e., camera preview-and-capture, we
+   respond to presses of the center trackball/mouse button by taking the
+   picture of whatever the camera is currently looking at (users should
+   be able to tell what they're going to get through the preview).
 
-	}
+   In case users press the back button, we return to the photo manager's
+   main screen without taking a picture using the camera.
+*/
+@Override public boolean onKeyDown(int key_code, KeyEvent event)
+{
+   if (key_code == KeyEvent.KEYCODE_DPAD_CENTER)
+   {
+      m_camera.takePicture(null, null, new ImageCaptureCallback()) ;
+      wind_up(RESULT_OK) ;
+      return true ;
+   }
+   if (key_code == KeyEvent.KEYCODE_BACK)
+   {
+      wind_up(RESULT_CANCELED) ;
+      return true ;
+   }
+   return super.onKeyDown(key_code, event) ;
+}
 
-	// -----------------------------------------------------------------------
+//-------------------------- CAMERA PREVIEW -----------------------------
+
+/**
+   This inner class implements the camera preview. It is pretty much
+   lifted verbatim from the Android CameraPreview sample.
+*/
+private class CameraPreview extends SurfaceView
+                            implements SurfaceHolder.Callback {
+   private SurfaceHolder m_holder ;
+
+   public CameraPreview(Context C) {
+      super(C) ;
+      m_holder = getHolder() ;
+      m_holder.addCallback(this) ;
+      m_holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS) ;
+   }
+
+   public void surfaceCreated(SurfaceHolder H) {
+      m_camera = Camera.open() ;
+      m_camera.setPreviewDisplay(H) ;
+   }
+
+   public void surfaceChanged(SurfaceHolder H, int format, int w, int h) {
+      Camera.Parameters params = m_camera.getParameters() ;
+      params.setPreviewSize(w, h) ;
+      m_camera.setParameters(params) ;
+      m_camera.startPreview() ;
+   }
+
+   public void surfaceDestroyed(SurfaceHolder H) {
+      m_camera.stopPreview() ;
+      m_camera.release() ;
+      m_camera = null ;
+   }
+} // end of inner class ImageRecorder.CameraPreview
+
+//---------------------- IMAGE CAPTURE CALLBACK -------------------------
+
+/**
+   This inner class is used to create bitmaps out of the picture data
+   Android obtains from the camera and store these bitmaps along with
+   their thumbnails to the MediaStore.Images database.
+*/
+private class ImageCaptureCallback implements Camera.PictureCallback {
+
+public void onPictureTaken(byte[] data, Camera camera)
+{
+   // Stuff the raw camera data into a bitmap image object and store it
+   // to the MediaStore.Images database.
+   Bitmap image = BitmapFactory.decodeByteArray(data, 0, data.length) ;
+   Media.insertImage(getContentResolver(), image, "cs546.group7",
+                     "Captured by cs546.group7.AssignmentThree") ;
+
+   //TODO: get cursor to above image
+   //TODO: update GPS columns and date
+   //TODO: include date in image title and description
+   //TODO: put thumbnail ID into m_thumbnail_id
+}
+
+} // end of class inner ImageRecorder.ImageCaptureCallback
+
+//-----------------------------------------------------------------------
 
 } // end of class cs546.group7.ImageRecorder
